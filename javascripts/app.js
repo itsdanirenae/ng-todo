028df3821bcd6a182825b341729a1cef148fe94e
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-15 20:17:56 -0600

    added day 2 files

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,56 +2,20 @@
 
 var app = angular.module("TodoApp", []);
 
-app.controller("NavCtrl", function($scope){
-  $scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
-})
+//BELOW: all the ways to call firebase
+// app.config(function(){
+// });
 
-app.controller("TodoCtrl", function($scope){
-  $scope.welcome = "hello";
-  $scope.showListView = true;
-  $scope.newTask = {};
-  $scope.items = [
-    {
-      id: 0,
-      task : "mow the lawn",
-      isCompleted: true,
-      assignedTo: "Dani"
-    },
-     {
-      id: 1,
-      task : "grade quizzes",
-      isCompleted: false,
-      assignedTo: "Zoe"
-    },
-     {
-      id: 2,
-      task : "take nap",
-      isCompleted: false,
-      assignedTo: "Nathan"
-    }
-  ];
+// app.run(function(){
+// });
 
-$scope.allItems = function(){
-  console.log("you clicked all items")
-  $scope.showListView = true;
-}
+// app.value("FB", valuehere) //value and constant won't work with JSON
 
-$scope.newItem = function(){
-  console.log("you clicked new item")
-  $scope.showListView = false;
-}
+// app.constant("FB", valuehere)
 
-$scope.addNewItem = function(){
-  $scope.newTask.isCompleted = false;
-  $scope.newTask.id = $scope.items.length;
-  console.log("new task in Function", $scope.newTask);
-  $scope.items.push($scope.newTask);
-  $scope.newTask = "";
-  $scope.showListView = true;
-}
+// $http.get() //what does this do?
 
 
-});
 
 
 
