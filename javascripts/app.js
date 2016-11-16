3f1770f9ee19fb840359e193c69173b243dce33d
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-15 18:56:50 -0600

    day one complete

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,6 +9,7 @@ app.controller("NavCtrl", function($scope){
 app.controller("TodoCtrl", function($scope){
   $scope.welcome = "hello";
   $scope.showListView = true;
+  $scope.newTask = {};
   $scope.items = [
     {
       id: 0,
@@ -33,12 +34,26 @@ app.controller("TodoCtrl", function($scope){
 $scope.allItems = function(){
   console.log("you clicked all items")
   $scope.showListView = true;
-
 }
+
 $scope.newItem = function(){
   console.log("you clicked new item")
   $scope.showListView = false;
-
 }
 
+$scope.addNewItem = function(){
+  $scope.newTask.isCompleted = false;
+  $scope.newTask.id = $scope.items.length;
+  console.log("new task in Function", $scope.newTask);
+  $scope.items.push($scope.newTask);
+  $scope.newTask = "";
+  $scope.showListView = true;
+}
+
+
 });
+
+
+
+
+
