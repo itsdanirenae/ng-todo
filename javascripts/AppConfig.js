dfc66c241f9a4711c539f23ab6130de694720070
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-19 11:38:26 -0600

    added day 3

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -4,3 +4,16 @@
 app.run(function(FIREBASE_CONFIG){
   firebase.initializeApp(FIREBASE_CONFIG);
 });
+
+app.config(function($routeProvider){ //routeProvider is angular method that does routes
+  $routeProvider
+    .when('/items/list', {
+      templateUrl: 'partials/item-list.html',  //this is important how these are spelled here
+      controller: 'ItemListCtrl'
+    })
+    .when('/items/new', {
+      templateUrl: 'partials/item-new.html',
+      controller: 'ItemNewCtrl'
+    })
+    .otherwise('/items/list')
+})
---
+++ javascripts/controllers/ItemListCtrl.js
@@ -0,0 +1,20 @@
+"use strict";
+
+app.controller("ItemListCtrl", function($scope, ItemFactory){
+  $scope.items = [];
+
+let getItems = function(){
+   ItemFactory.getItemList().then(function(fbItems){
+    $scope.items = fbItems;
+  });
+}
+getItems();
+
+$scope.deleteItem = function(itemId){ //passed this in on click here and in HTML - we delete item.id in html and this is why we created items with ids
+  console.log("you deleted me");
+  ItemFactory.deleteItem(itemId).then(function(response){
+    getItems(); //once it is deleted then refresh the DOM
+  })
+}
+
+});
