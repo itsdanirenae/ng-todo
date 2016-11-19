d2c0915057e099d19fd6ca2524b7e670f9bd2723
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-19 14:07:30 -0600

    completed day 3

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -15,5 +15,13 @@ app.config(function($routeProvider){ //routeProvider is angular method that does
       templateUrl: 'partials/item-new.html',
       controller: 'ItemNewCtrl'
     })
+    .when('/items/view/:id', { //putting a : in front tells us that it will change with dif id (can be named anything)
+      templateUrl: 'partials/item-view.html',
+      controller: 'ItemViewCtrl'
+    })
+    .when('/items/edit/:id', {
+      templateUrl: 'partials/item-new.html',
+      controller: 'ItemEditCtrl'
+    })
     .otherwise('/items/list')
-})
+});
