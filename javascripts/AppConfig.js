99b535f7efe02e66a73ccd53a4a921a427e8f581
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-22 20:53:26 -0600

    day 4

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,8 +1,39 @@
 "use strict";
 
+//function that tell us whether or not a person is logged in
+let isAuth = (AuthFactory) => new Promise((resolve, reject)=>{
+  if(AuthFactory.isAuthenticated()){
+    resolve();
+  } else {
+    reject();
+  }
+});
 
-app.run(function(FIREBASE_CONFIG){
+
+
+app.run(function($rootScope, $location, FIREBASE_CONFIG, AuthFactory){
   firebase.initializeApp(FIREBASE_CONFIG);
+
+  $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute){
+
+    let logged = AuthFactory.isAuthenticated();
+    let appTo;
+
+if(currRoute.originalPath){
+    appTo = 5 !== -1 //true
+    appTo = -1 !== -1 //false
+    appTo = currRoute.originalPath.indexOf('/auth') !== -1;
+}
+
+    console.log("appTo", appTo);
+    console.log("hey");
+
+    if(!appTo && !logged){
+      event.preventDefault();
+      $location.path('/auth');
+
+    }
+  });
 });
 
 app.config(function($routeProvider){ //routeProvider is angular method that does routes
@@ -13,19 +44,28 @@ app.config(function($routeProvider){ //routeProvider is angular method that does
     })
     .when('/items/list', {
       templateUrl: 'partials/item-list.html',  //this is important how these are spelled here
-      controller: 'ItemListCtrl'
+      controller: 'ItemListCtrl',
+      resolve: {isAuth} // if isAuth is true then load this controller/partial
     })
     .when('/items/new', {
       templateUrl: 'partials/item-new.html',
-      controller: 'ItemNewCtrl'
+      controller: 'ItemNewCtrl',
+      resolve: {isAuth}
     })
     .when('/items/view/:id', { //putting a : in front tells us that it will change with dif id (can be named anything)
       templateUrl: 'partials/item-view.html',
-      controller: 'ItemViewCtrl'
+      controller: 'ItemViewCtrl',
+      resolve: {isAuth}
     })
     .when('/items/edit/:id', {
       templateUrl: 'partials/item-new.html',
-      controller: 'ItemEditCtrl'
+      controller: 'ItemEditCtrl',
+      resolve: {isAuth}
     })
-    .otherwise('/items/list');
+    .when('/logout', {
+      templateUrl: 'partials/auth.html',
+      controller: 'AuthCtrl',
+      resolve: {isAuth}
+    })
+    .otherwise('/auth');
 });
--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -5,6 +5,13 @@ app.controller("AuthCtrl", function($scope, $rootScope, ItemFactory, AuthFactory
   $scope.loginContainer = true;
   $scope.registerContainer = false;
 
+
+  if($location.path() === "/logout"){
+    AuthFactory.logout();
+    $rootScope.user = {};
+    $location.url("/auth");
+  }
+
 let logMeIn = function(loginStuff){
   AuthFactory.authenticate(loginStuff).then(function(didLogin){
     console.log(didLogin);
